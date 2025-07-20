81dac5a3f1287626572030113837f817b3647b27
Angel Oliver <angel.oliver.cv@gmail.com>
2025-07-20 18:12:17 -0300

    eliminar la implementación del módulo de productos

--- src/_app/_back/product/infra/product.document.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-
-export interface ProductDocument extends Document {
-  name: string;
-  price: number;
-  stock: number;
-}
--- src/_app/_back/routes.ts
+++
@@ -1,7 +0,0 @@
-import { productRoutes } from "@/_app/_back/product/routes";
-import { agencyRoutes } from "@/_agency/_back/agency/routes";
-
-export const routes = {
-  ...productRoutes,
-  ...agencyRoutes,
-};
